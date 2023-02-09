048a050ee8de82ad9b1e0dc6a87d5b4b3d4dd9d3
Alex De Meo <alexdm9572@gmail.com>
2023-02-09 02:31:18 +0000

    Style checked

---
+++ HelloWorldStyle.java
@@ -0,0 +1,28 @@
+/**
+* This program displays text to the user.
+*
+* @author  Alex De Meo
+* @version 1.0
+* @since   2023/02/08
+*/
+public final class HelloWorldStyle {
+    /**
+    * This is a private constructor used to satisfy the
+    * style checker.
+    *
+    * @exception IllegalStateException Utility Class
+     */
+    private HelloWorldStyle() {
+        throw new IllegalStateException("Utility Class");
+    }
+    /**
+    * This is the main method.
+    *
+    * @param args Unused
+    */
+
+    public static void main(String[] args) {
+        System.out.println("Helllllo");
+        System.out.println("Hello everybody");
+    }
+}
